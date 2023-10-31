f073797e2efdb6629f1c95847fc6fddb515acb65
Luis Gaudencio <luis_g24@live.com>
2023-10-31 22:16:26 +0000

    Add allauth templates directory to settings.py

--- bringabackpack/settings.py
+++ bringabackpack/settings.py
@@ -19,6 +19,7 @@ if os.path.isfile('env.py'):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+TEMPLATES_ALLAUTH_DIR = os.path.join(BASE_DIR, 'templates', 'allauth')
 
 
 # Quick-start development settings - unsuitable for production
@@ -70,7 +71,10 @@ ROOT_URLCONF = 'bringabackpack.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [
+            TEMPLATES_DIR,
+            TEMPLATES_ALLAUTH_DIR
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
