5ca269390dcef07abf3ddaf95623d915e095fc91
Luis Gaudencio <luis_g24@live.com>
2023-11-07 22:17:24 +0000

    Add destination model into admin

--- destinations/admin.py
+++ destinations/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Destination
 
-# Register your models here.
+@admin.register(Destination)
+class DestinationAdmin(admin.ModelAdmin):
+    list_display = (
+        'title', 
+        'brief', 
+        'photo',
+        'review'
+    )
+
