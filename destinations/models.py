90dd8a2473012c1c03062f0e64c407e6bc331777
Luis Gaudencio <luis_g24@live.com>
2023-11-07 21:22:54 +0000

    Add user, title and brief fields to model

--- destinations/models.py
+++ destinations/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Destination(models.Model):
     """
     A model to create and manage all destination reviews
     """
+    user = models.ForeignKey(User, related_name='destination_review_owner', on_delete=models.CASCADE)
+    title = models.CharField(max_length=250, null=False, blank=False)
+    brief = models.CharField(max_length=450, null=False, blank=False)
