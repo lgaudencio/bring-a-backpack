431236482b1334004e31e03dd7a09a974500e5a3
Luis Gaudencio <luis_g24@live.com>
2023-11-07 21:49:32 +0000

    Add photo, photo alt, review and date posted as fields in the model

--- destinations/models.py
+++ destinations/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from djrichtextfield.models import RichTextField
+from django_resized import ResizedImageField 
+
 class Destination(models.Model):
     """
     A model to create and manage all destination reviews
@@ -8,3 +11,9 @@ class Destination(models.Model):
     user = models.ForeignKey(User, related_name='destination_review_owner', on_delete=models.CASCADE)
     title = models.CharField(max_length=250, null=False, blank=False)
     brief = models.CharField(max_length=450, null=False, blank=False)
+    photo = ResizedImageField(
+        size=[500, None], quality=75, upload_to='destinations/', force_format='WEBP', null=False,
+        blank=False
+    )
+    photo_alt = models.CharField(max_length=100, null=False, blank=False)
+    review = RichTextField(max_length=20000, null=False, blank=False)
