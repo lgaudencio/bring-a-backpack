a69d24d166a6bce431be19f763230b9e141f4c4e
Luis Gaudencio <luis_g24@live.com>
2023-11-08 22:20:50 +0000

    Add url path for richtextfield

--- bringabackpack/urls.py
+++ bringabackpack/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('reviewsite.urls')),
     path('accounts/', include('allauth.urls')),
+    path('djrichtextfield/', include('djrichtextfield.urls'))
 ]
