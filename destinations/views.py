f02e289afa9897719a2e266cff1aa516f055fa04
Luis Gaudencio <luis_g24@live.com>
2023-11-10 22:21:04 +0000

    Add form valid to add destination class

--- destinations/views.py
+++ destinations/views.py
@@ -10,3 +10,7 @@ class AddDestination(CreateView):
     template_name = 'destinations/add_destination.html'
     model = Destination
     success_url = '/destinations/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(AddDestination, self).form_valid(form)
