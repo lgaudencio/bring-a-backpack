a541cccc20acc4f51406bea38b6fd8a37639491a
Luis Gaudencio <luis_g24@live.com>
2023-11-14 21:31:19 +0000

    Add url path for destinations

--- destinations/urls.py
+++ destinations/urls.py
@@ -4,4 +4,5 @@ from .views import AddDestination, Destinations
 
 urlpatterns = [
     path('', AddDestination.as_view(), name='add_destination'), 
+    path('destinations/', Destinations.as_view(), name='destinations'),
 ]
