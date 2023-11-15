312cf6ec077ff85e3129c8d7f966cb78d1696261
Luis Gaudencio <luis_g24@live.com>
2023-11-15 21:45:08 +0000

    Add url path for destination full review

--- destinations/urls.py
+++ destinations/urls.py
@@ -5,4 +5,5 @@ from .views import AddDestination, Destinations, DestinationFullReview
 urlpatterns = [
     path('', AddDestination.as_view(), name='add_destination'), 
     path('destinations/', Destinations.as_view(), name='destinations'),
+    path('<slug:pk>/', DestinationFullReview.as_view(), name='destination_full_review'),
 ]
