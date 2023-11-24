1dfd7153cc328dc4301885f06738e9e7f6d7a17b
Luis Gaudencio <luis_g24@live.com>
2023-11-24 22:26:07 +0000

    Add profile class to models

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from djrichtextfield.models import RichTextField
+from django_resized import ResizedImageField
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver 
+
+class Profile(models.Model):
+    """
+    Profile model
+    """
+    user = models.ForeignKey(User, related_name="profile", on_delete=models.CASCADE)
+    display_picture = ResizedImageField(size=[250,250], quality=75, upload_to="profiles/", force_format="WEBP", blank=False)
+    nationality = RichTextField(max_length=50, null=False, blank=False)
+    traveler_type = RichTextField(max_length=100, null=False, blank=False)
+    bio = RichTextField(max_length=2000, null=True, blank=True)
