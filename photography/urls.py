7116ed4c4baa8c31067725a18571cf790dfaec82
Luis Gaudencio <luis_g24@live.com>
2023-12-04 22:00:30 +0000

    Add url path for the photography detail view

--- photography/urls.py
+++ photography/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import AddPhotography, PhotographyList
+from .views import AddPhotography, PhotographyList, PhotographyDetail
 
 
 urlpatterns = [
     path('', AddPhotography.as_view(), name='add_photography'),
-    path('photography_list/', PhotographyList.as_view(), name='photography_list')
+    path('photography_list/', PhotographyList.as_view(), name='photography_list'),
+    path('<slug:pk>/', PhotographyDetail.as_view(), name='photography_full_post'), 
 ]
