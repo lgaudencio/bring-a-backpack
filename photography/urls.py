ecd13f71f9ff471c0683ae8133e805f9e84c9c93
Luis Gaudencio <luis_g24@live.com>
2023-12-04 21:24:04 +0000

    Add url path for photography list view

--- photography/urls.py
+++ photography/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import AddPhotography
+from .views import AddPhotography, PhotographyList
 
 
 urlpatterns = [
-    path('', AddPhotography.as_view(), name='add_photography')
+    path('', AddPhotography.as_view(), name='add_photography'),
+    path('photography_list/', PhotographyList.as_view(), name='photography_list')
 ]
