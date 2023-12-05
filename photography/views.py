209d37f87b3a7dccde052e9514056be4087be221
Luis Gaudencio <luis_g24@live.com>
2023-12-05 22:23:23 +0000

    Add edit photography class for update view

--- photography/views.py
+++ photography/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import CreateView, ListView, DetailView, DeleteView
+from django.views.generic import CreateView, ListView, DetailView, DeleteView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from .models import Photography
@@ -35,6 +35,19 @@ class AddPhotography(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(AddPhotography, self).form_valid(form)
+
+
+class EditPhotography(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """ 
+    Edit a photography post
+    """
+    template_name = 'photography/edit_photography.html'
+    model = Photography
+    form_class = PhotographyForm
+    success_url = '/photography/'
+
+    def test_func(self):
+        return self.request.user == self.get_object().user
 
 
 class DeletePhotography(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
