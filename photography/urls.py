5e15c4a79366f51bcf7fc0376f8f5b90af1058f9
Luis Gaudencio <luis_g24@live.com>
2023-12-05 21:45:48 +0000

    Add url path for delete photography

--- photography/urls.py
+++ photography/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import AddPhotography, PhotographyList, PhotographyDetail
+from .views import AddPhotography, PhotographyList, PhotographyDetail, DeletePhotography
 
 
 urlpatterns = [
     path('', AddPhotography.as_view(), name='add_photography'),
     path('photography_list/', PhotographyList.as_view(), name='photography_list'),
-    path('<slug:pk>/', PhotographyDetail.as_view(), name='photography_full_post'), 
+    path('<slug:pk>/', PhotographyDetail.as_view(), name='photography_full_post'),
+    path('delete/<slug:pk>/', DeletePhotography.as_view(), name='delete_photography'), 
 ]
