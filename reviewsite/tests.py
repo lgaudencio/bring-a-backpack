2c0828b82594ec7832547e1a552d23f3508b8f33
Luis Gaudencio <luis_g24@live.com>
2024-01-12 18:06:57 +0000

    Add unit test for reviewsite app

--- reviewsite/tests.py
+++ reviewsite/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestViews(TestCase):
+    """
+    Test Reviewsite app
+    """
+    def test_home_page(self):
+        """ Test that the home page renders correctly """
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'reviewsite/index.html')
