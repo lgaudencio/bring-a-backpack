9a827dbc7723376477772ab4a9c2db044c3e5940
Luis Gaudencio <luis_g24@live.com>
2024-01-15 14:40:09 +0000

    Fix settings file to be pep8 compliant

--- bringabackpack/settings.py
+++ bringabackpack/settings.py
@@ -158,11 +158,11 @@ if 'DATABASE_URL' in os.environ:
     }
 else:
     DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
